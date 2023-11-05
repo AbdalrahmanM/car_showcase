0f0e276e61f051d92f01d216999ada0db073f5fc
AbdalrahmanM <abdodj18@gmail.com>
2023-11-05 20:07:56 +0300

    Refactor: Solve page.tsx sections an error

--- app/page.tsx
+++ app/page.tsx
@@ -85,7 +85,7 @@ export default function Home() {
         ) : (
           <div className="home__error-container">
             <h2 className="text-black text-xl font-bold">Oops , no results</h2>
-            <p>{allCars?.message}</p>
+            <p>No cars found.</p>
           </div>
         )}
       </div>
